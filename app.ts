20c4ce67a4ed1207b429a74dfb4e3f49dc9b6ddf
its-huzi-18 <157364561+its-huzi-18@users.noreply.github.com>
2024-03-30 23:13:25 +0500

    myFirst-commit

---
+++ app.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todosList = [];
+let againQuestion = true;
+while(againQuestion)
+{
+ 
+let addTask = await inquirer.prompt([
+    {
+        name:"todos",
+        type:"input",
+        message:"Welcome in todos app!What do you want to add in your todos list",
+    },
+    {
+        name:"addMore",
+        type:"confirm",
+        message:"Do you want to add more in your todos list ?",
+        default:"false",
+    },
+]);
+todosList.push(addTask.todos);
+againQuestion = addTask.addMore;
+console.log("List = ",todosList)
+    }
